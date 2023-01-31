39956763e1ad0f0781e3dba52849e7c935eb9dfc
탁승은 <xkrtmddms@gmail.com>
2023-01-31 09:53:10 +0900

    Initial commit

---
+++ src/main1/Main1.java
@@ -0,0 +1,66 @@
+package main1;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// 원시 타입
+		// 정수형 byte, short, int, long
+		// 실수형 float, double
+		// 문자형 char
+		// 논리형 boolean
+		
+		//클래스
+		// 정수형 Byte, Short, Integer, Long => 클래스로 만들어 놓은 것 
+		
+		
+		int age=13;
+		
+		//조건문
+		// if(조건) 비교: >= <= == !=
+		// if(조건 && 조건) AND: &&, OR: ||, NOT: !
+		if(age >= 50) {
+			System.out.println("50보다 크거나 같습니다");
+		}else {
+			System.out.println("50보다 작습니다");
+		}
+			
+		
+		// 반복문: for, while, do while
+		int i = 0;	//반복문 임시 변수
+		for(i=1; i<=5; i++) {// 1 2 3 4 5 6(X) => 6까지 가고 탈출함
+			System.out.print(i+" ");	// 출력 => 1 2 3 4 5
+		}
+		
+		System.out.println();
+		
+		// 반복문+조건문(1) beak;
+		for(i=1; i<=1000; i++) {	// 1부터 1001까지 가는데 1001에서 탈출
+			if(i>=10) {	// i가 10이 됐을 때
+				break;  // 반복문 강제 종료시킴	 10일때 종료 11부터는 수행X
+				//break;의 역할은 강제종료
+			}
+		}
+		
+		// 반복문+조건문(2) continue;
+		// 1~10까지 홀수 출력
+		for(i=1; i<=10; i++) {
+			if(i%2==0) {	//짝수일 경우에
+				continue;	// 현위치를 기준으로 아래 소스코드는 수행되지 않음
+			}
+			System.out.print(i+" ");
+		}
+		
+		
+		System.out.println();
+		
+		System.out.println(Integer.SIZE);	// 32bit => 4byte
+		System.out.println(Double.SIZE);
+		
+		System.out.println("결과는" + 13);	//여기서는 String을 쓴것
+		
+		
+
+
+	}
+
+}
---
+++ src/main1/Main2.java
@@ -0,0 +1,67 @@
+package main1;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		// 배열의 위치는 0부터 시작한다 0 1 2 3 4 이런식으로
+		// 맨 마지막 위치는 (전체개수-1)이다  
+		int i; // 반복문 임시변수
+		int[] age = {1,2,3,4,5}; //나이 변수가 5개 생성
+		// 위의 코드가 밑과 동일하다
+		/* int age1 = 1;
+		 * int age2 = 2;
+		 * int age3 = 3;
+		 * int age4 = 4;
+		 * int age5 = 5;
+		 */
+
+		System.out.println(age[0]);
+		System.out.println(age[4]);
+	
+	
+		for(i=0; i<age.length;i++) {
+			System.out.println(age[i]);
+		}
+		System.out.println();
+		
+		//age의 값들을 반대로 출력하세요
+		
+		for(i=age.length-1; i>=0; i--) {
+			System.out.print(age[i]);
+		}
+		
+		System.out.println();
+		
+		for(i=0; i<age.length;i++) {
+			System.out.print(age[4-i] + " ");
+		}
+		
+		System.out.println();
+		
+		// age의 값들의 합과 평균 구하기
+		int sum=0;
+		float avg=0;
+		
+		for(i=0; i<age.length;i++) {
+			sum += age[i];
+		}
+		System.out.print("합:"+sum);
+		System.out.println();
+		avg = sum/age.length;
+		System.out.println("평균:"+avg);
+		
+		
+		System.out.println();
+		
+		// age 5개 중에서 짝수만 출력
+
+		for (i = 0; i < age.length; i++) {
+			if (age[i] % 2 == 0) 
+				System.out.print(age[i]);
+
+		}
+	
+	}
+
+}
