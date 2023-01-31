a52dabaf899c524060429df8602c419d1a5da613
탁승은 <xkrtmddms@gmail.com>
2023-01-31 13:50:43 +0900

    클래스
    클래스

--- src/cls/Board.java
+++ src/cls/Board.java
@@ -2,6 +2,14 @@ package cls;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
 //글번호(long) brdNo, 글제목(String)brdTitle
 //글내용(String)brdContent, 작성자(String)brdWriter
 //조회수(long)brdHit, 등록일자(Date)brdDate 
@@ -10,9 +18,6 @@ import java.util.Date;
 
 
 public class Board {
-	
-	
-	
 	
 	
 	//private 외부에서 사용하지 말라고 쓰는 것
@@ -25,48 +30,48 @@ public class Board {
 	private Date brdDate = null; //날짜
 	
 	
-	@Override
-	public String toString() {
-		return "Board [brdNo=" + brdNo + ", brdTitle=" + brdTitle + ", brdContent=" + brdContent + ", brdWriter="
-				+ brdWriter + ", brdHit=" + brdHit + ", brdDate=" + brdDate + "]";
-	}
-	
-	public long getBrdNo() {
-		return brdNo;
-	}
-	public void setBrdNo(long brdNo) {
-		this.brdNo = brdNo;
-	}
-	public String getBrdTitle() {
-		return brdTitle;
-	}
-	public void setBrdTitle(String brdTitle) {
-		this.brdTitle = brdTitle;
-	}
-	public String getBrdContent() {
-		return brdContent;
-	}
-	public void setBrdContent(String brdContent) {
-		this.brdContent = brdContent;
-	}
-	public String getBrdWriter() {
-		return brdWriter;
-	}
-	public void setBrdWriter(String brdWriter) {
-		this.brdWriter = brdWriter;
-	}
-	public long getBrdHit() {
-		return brdHit;
-	}
-	public void setBrdHit(long brdHit) {
-		this.brdHit = brdHit;
-	}
-	public Date getBrdDate() {
-		return brdDate;
-	}
-	public void setBrdDate(Date brdDate) {
-		this.brdDate = brdDate;
-	}
-	
+//	@Override
+//	public String toString() {
+//		return "Board [brdNo=" + brdNo + ", brdTitle=" + brdTitle + ", brdContent=" + brdContent + ", brdWriter="
+//				+ brdWriter + ", brdHit=" + brdHit + ", brdDate=" + brdDate + "]";
+//	}
+//	
+//	public long getBrdNo() {
+//		return brdNo;
+//	}
+//	public void setBrdNo(long brdNo) {
+//		this.brdNo = brdNo;
+//	}
+//	public String getBrdTitle() {
+//		return brdTitle;
+//	}
+//	public void setBrdTitle(String brdTitle) {
+//		this.brdTitle = brdTitle;
+//	}
+//	public String getBrdContent() {
+//		return brdContent;
+//	}
+//	public void setBrdContent(String brdContent) {
+//		this.brdContent = brdContent;
+//	}
+//	public String getBrdWriter() {
+//		return brdWriter;
+//	}
+//	public void setBrdWriter(String brdWriter) {
+//		this.brdWriter = brdWriter;
+//	}
+//	public long getBrdHit() {
+//		return brdHit;
+//	}
+//	public void setBrdHit(long brdHit) {
+//		this.brdHit = brdHit;
+//	}
+//	public Date getBrdDate() {
+//		return brdDate;
+//	}
+//	public void setBrdDate(Date brdDate) {
+//		this.brdDate = brdDate;
+//	}
+//	
 	
 }
